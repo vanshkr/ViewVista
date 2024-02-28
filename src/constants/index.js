d5afe64c33c0db783e163babbb7bfb5071b6f24f
Vansh Kumar <krvansh30@gmail.com>
2024-02-28 19:42:36 +0530

    fixes in route

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,7 +11,7 @@ export const sidebarLinks = [
   },
   {
     imgURL: "/assets/icons/people.svg",
-    route: "/all-users",
+    route: "/allUsers",
     label: "People",
   },
   {
@@ -21,7 +21,7 @@ export const sidebarLinks = [
   },
   {
     imgURL: "/assets/icons/gallery-add.svg",
-    route: "/create-post",
+    route: "/createPost",
     label: "Create Post",
   },
 ];
@@ -44,7 +44,7 @@ export const bottombarLinks = [
   },
   {
     imgURL: "/assets/icons/gallery-add.svg",
-    route: "/create-post",
+    route: "/createPost",
     label: "Create",
   },
 ];
