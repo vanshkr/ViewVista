ccdb11ac94ca3ddc4090280cb244b24e0fa37810
Vansh Kumar <krvansh30@gmail.com>
2024-02-28 19:31:40 +0530

    route & pag setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./_auth/form/SignIn";
 import SignUp from "./_auth/form/SignUp";
-import { Home } from "./_root/pages/index.js";
+import {
+  Home,
+  Explore,
+  Saved,
+  CreatePost,
+  Profile,
+  EditPost,
+  PostDetails,
+  UpdateProfile,
+  AllUsers,
+} from "@/_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
 import RootLayout from "./_root/RootLayout.jsx";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,15 +21,23 @@ function App() {
   return (
     <main className="flex h-screen">
       <Routes>
-        // private routes
+        // public routes
         <Route element={<AuthLayout />}>
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/signUp" element={<SignUp />} />
         </Route>
-        // public routes /* index - allows child component to be rendered on the
-        same path as parent */
+        // private routes /* index - allows child component to be rendered on
+        the same path as parent */
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/allUsers" element={<AllUsers />} />
+          <Route path="/createPost" element={<CreatePost />} />
+          <Route path="/updatePost/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/updateProfile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
       <Toaster />
